80c9d9fb222fc7345084e196e0e7bf0ed112f7bf
ROHIT KUMAR CHANDOLIYA <115701820+ravan8853558@users.noreply.github.com>
2026-02-23 16:32:37 +0530

    Update server.cjs

--- Event-Certificates-Automation/backend/server.cjs
+++ Event-Certificates-Automation/backend/server.cjs
@@ -77,7 +77,13 @@ app.use(cors({
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static(UPLOAD_DIR));
+app.use(
+  "/uploads",
+  cors({
+    origin: "*"
+  }),
+  express.static(UPLOAD_DIR)
+);
 
 const loginLimiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 10 });
 const submitLimiter = rateLimit({ windowMs: 60 * 1000, max: 5 });
